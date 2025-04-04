c9338f05e729f164bfdd2d5924ab29d4371081e9
Maicol <maicolmike@hotmail.com>
2025-04-04 15:46:31 -0500

    bd

--- conveniosDj/views.py
+++ conveniosDj/views.py
@@ -4,6 +4,14 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required # vista basada en funciones que no permita acceder a paginas donde no se ha logeado
 from django.shortcuts import redirect
 
-def index(request):
+'''def index(request):
     # Redirige automáticamente a la página de consulta
-    return redirect('consulta')  # Cambia 'consulta' al nombre correcto de tu URL
+    return redirect('consulta')  # Cambia 'consulta' al nombre correcto de tu URL  '''
+
+
+@login_required(login_url='login')
+def index(request):
+    #return HttpResponse('Hola mundo')
+    return render(request, 'index.html',{ 
+        'title': "Index",
+    })
--- registro/views.py
+++ registro/views.py
@@ -3,16 +3,23 @@ from .models import Equipo
 from .forms import EquipoForm
 
 
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def registrar_equipo(request):
     if request.method == 'POST':
         form = EquipoForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('lista_equipos')  # Redirige a la vista de equipos
+            equipo = form.save(commit=False)
+            equipo.funcionario_registra = request.user.get_full_name() or request.user.username  # Usa nombre completo si está disponible
+            equipo.save()
+            return redirect('lista_equipos')
     else:
         form = EquipoForm()
     return render(request, 'registro/registrar_equipo.html', {'form': form})
 
+
+@login_required
 def lista_equipos(request):
     equipos = Equipo.objects.all()
     return render(request, 'registro/lista_equipos.html', {'equipos': equipos})
