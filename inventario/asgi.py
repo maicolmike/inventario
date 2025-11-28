98544de3cc988995d7dfe56285df00940108d883
Maicol <maicolmike@hotmail.com>
2025-11-28 21:04:34 +0000

    cambio de nombre del proyecto

--- inventario/asgi.py
+++ inventario/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conveniosDj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventario.settings')
 
 application = get_asgi_application()
